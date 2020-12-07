a39f1825b6a345ae60c3b8e1cf12f091b0d05285
Mikkel Lysgård Andersen <man@magnetix.dk>
2020-12-07 14:40:13 +0100

    update

--- bannerapp/src/config/config.js
+++ bannerapp/src/config/config.js
@@ -1,6 +1,6 @@
 import config from 'react-global-configuration';
 
 config.set({
-    apiRoot: "https://dwigt.github.io/banners/bannerapi"
-        // apiRoot: "http://localhost:8000"
+    // apiRoot: "https://dwigt.github.io/banners/bannerapi"
+        apiRoot: "http://localhost:8000"
 });
